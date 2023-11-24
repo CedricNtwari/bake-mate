3a944e60c4b7e77c9be49decf28c02ff55383a95
CedricNtwari <>
2023-11-24 16:25:56 +0000

    Create get cake recipes function

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+import pprint
+
+API_KEY = 'dd6079b14eee42dba13099a4eeff6317'
+
+
+def get_cake_recipes(api_key, query='cake'):
+    """
+    Define the base URL for Spoonacular API
+    """
+    base_url = 'https://api.spoonacular.com/recipes/search'
+    # Set up parameters for the API request
+    params = {
+        'apiKey': api_key,
+        'query': query,
+        'type': 'cake',
+        'number': 5  # number of recipes to retrieve
+    }
+
+    # Make a GET request to the Spoonacular API
+    response = requests.get(base_url, params=params)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        recipes_data = response.json()
+        # Return the 'results' part of the response, which contains recipe data
+        pprint.pprint(recipes_data)
+        return recipes_data['results']
+    else:
+        # Print an error message if the request was not successful
+        print(f"Error {response.status_code}: Unable to fetch recipes.")
+        return None
+
