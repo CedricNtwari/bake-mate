6298a94acbbf00617a8200f78bb210d66c338b1c
CedricNtwari <>
2023-12-06 17:15:55 +0000

    Allow app to link style & script files

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from config import API_KEY
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 FOOD2FORK_API_URL = 'https://food2fork.ca/api/recipe/search/'
 
