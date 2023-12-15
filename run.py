588244cc569f2fa5875ee06aabcbf024db5ccaf0
CedricNtwari <>
2023-12-15 14:25:17 +0000

    Add error page

--- run.py
+++ run.py
@@ -125,5 +125,15 @@ def load_more():
         return "Error fetching more recipes."
 
 
+# Custom 404 error handler
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error_404.html'), 404
+
+# General error handler for other HTTP error codes
+@app.errorhandler(500)
+def internal_server_error(e):
+    return "Internal Server Error", 500
+
 if __name__ == '__main__':
     app.run(debug=True)
