06a9cdac557660e148182ec0560a452098fd9b8c
CedricNtwari <>
2024-01-18 22:39:02 +0000

    Toggle edit panel and update ingredient list

--- static/js/script.js
+++ static/js/script.js
@@ -16,3 +16,35 @@ function showScrollUpButton() {
         scrollUpButton.style.display = 'none';
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const editButtons = document.querySelectorAll('.edit-btn');
+
+    editButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const editPanel = this.nextElementSibling;
+            editPanel.classList.toggle('show-edit-panel');
+        });
+    });
+
+    const updateButtons = document.querySelectorAll('.update-btn');
+
+    updateButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const editPanel = this.parentElement;
+            const inputField = editPanel.querySelector('.edit-input');
+            const quantityNameSpan = this.closest('.ingredient-item').querySelector('.ingredient-quantity-name');
+
+            if (inputField.value.trim() !== '') {
+                // Update the quantity in the sentence
+                const newQuantity = inputField.value.trim();
+                const currentIngredient = quantityNameSpan.textContent;
+                const updatedIngredient = currentIngredient.replace(/\d+(\.\d+)?/, newQuantity);
+                quantityNameSpan.textContent = updatedIngredient;
+
+                // Hide the edit panel
+                editPanel.classList.remove('show-edit-panel');
+            }
+        });
+    });
+});
